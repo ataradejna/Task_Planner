5853707ed9a342afae99cb77b562f9a2b4c5c133
Agnieszka Taradejna <agnieszka.taradejna@gmail.com>
2021-05-08 14:36:26 +0200

    addin a new task function and showing if task is done

--- js/script.js
+++ js/script.js
@@ -13,19 +13,45 @@
     const render = () => {
         let htmlString = "";
 
-        for (const task of tasks) {
+        for(const task of tasks){
             htmlString += `
-            <li>
-                ${task.content}
-            </li>
-            `; 
-         }
-
-         document.querySelector(".js-tasksList").innerHTML = htmlString;
+                <li class="list__item${task.done ? " list__item--done " : ""}"
+                >
+                    ${task.content}
+                </li>
+                `;
+        }
+        
+        document.querySelector(".js-list").innerHTML = htmlString;
     };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        }); 
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if (newTaskContent === "") {
+            return;
+        }
+         
+        addNewTask(newTaskContent);
+    };
+
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form")
+
+        form.addEventListener("submit", onFormSubmit)
     };
 
     init();
